9cb28cec6822c5eac721444601a6b91475cc797d
Sargy <lukina_sargy@mail.ru>
2020-03-10 17:50:34 +0300

    163 connecting state to build controls

--- src/components/Burger/BuildControls/BuildControl/BuildControl.js
+++ src/components/Burger/BuildControls/BuildControl/BuildControl.js
@@ -2,10 +2,10 @@ import React from "react";
 import classes from './BuildControl.module.css';
 
 const BuildControl = (props) => (
-    <div>
-        <div className={classes.Name}>{props.name}</div>
+    <div className={classes.BuildControl}>
+        <div className={classes.Label}>{props.label}</div>
         <button className={classes.Less}>Less</button>
-        <button className={classes.More}>More</button>
+        <button className={classes.More} onClick={props.added}>More</button>
     </div>
 );
 export default BuildControl;
